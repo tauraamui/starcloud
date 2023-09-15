13b2c382b118725e6607147061651a976c6b0c54
tauraamui <adampstringer@protonmail.com>
2023-09-15 11:18:39 +0100

    feat: ensure toolbar controls awareness of active buttons

--- src/widgets/toolbar.v
+++ src/widgets/toolbar.v
@@ -18,9 +18,9 @@ mut:
 	is_pressed bool
 }
 
-pub fn (button Button) draw(ops op.Stack, gfx &gg.Context) {
+pub fn (button Button) draw(ops op.Stack, gfx &gg.Context, active bool) {
 	min := button.area.min.offset(ops)
-	gfx.draw_rounded_rect_filled(min.x, min.y, button.area.max.x, button.area.max.y, 9, gx.rgb(172, 155, 238))
+	gfx.draw_rounded_rect_filled(min.x, min.y, button.area.max.x, button.area.max.y, 9, if active { gx.rgb(172, 155, 238) } else { gx.rgb(3, 3, 3) } )
 
 	// NOTE:(tauraamui) -> not really sure what I am doing here,
 	//                     I think I am summing an alpha color shaded
@@ -48,12 +48,13 @@ pub fn (button Button) draw(ops op.Stack, gfx &gg.Context) {
 	})
 }
 
-fn (mut button Button) on_event(ops op.Stack, e &gg.Event) bool {
+fn (mut button Button) on_event(ops op.Stack, e &gg.Event, id int, update_active fn(int), active bool) bool {
+	min := button.area.min.offset(ops)
 	match e.typ {
 		.mouse_down {
 			if e.mouse_button == gg.MouseButton.left {
-				min := button.area.min.offset(ops)
 				if contains_point(min, button.area.max, Pt{ x: e.mouse_x / gg.dpi_scale(), y: e.mouse_y / gg.dpi_scale() }) {
+					if active { return false }
 					sapp.set_mouse_cursor(sapp.MouseCursor.pointing_hand)
 					button.is_pressed = true
 					return true
@@ -67,7 +68,9 @@ fn (mut button Button) on_event(ops op.Stack, e &gg.Event) bool {
 			sapp.set_mouse_cursor(sapp.MouseCursor.default)
 			if button.is_pressed {
 				button.is_pressed = false
-				println("unpressed")
+				if contains_point(min, button.area.max, Pt{ x: e.mouse_x / gg.dpi_scale(), y: e.mouse_y / gg.dpi_scale() }) {
+					update_active(id)
+				}
 			}
 		}
 		else {}
@@ -87,6 +90,7 @@ pub struct Toolbar {
 pub:
 	area Span
 mut:
+	active_button_id int
 	buttons []Button
 }
 
@@ -94,6 +98,7 @@ pub fn Toolbar.new(ass assets.Assets) Toolbar {
 	return Toolbar{
 		area: widgets.Span{ min: widgets.Pt{0, 8}, max: widgets.Pt{312.5, 38} }
 		buttons: [
+			Button{ assets: ass, area: Span{ min: Pt{ 0, 0 }, max: Pt{ x: 30, y: 28 } } }
 			Button{ assets: ass, area: Span{ min: Pt{ 0, 0 }, max: Pt{ x: 30, y: 28 } } }
 		]
 	}
@@ -110,20 +115,35 @@ pub fn (mut toolbar Toolbar) draw(mut ops op.Stack, mut gfx &gg.Context) {
 	defer { ops.pop_offset() }
 	ops.push_offset(5, 5)
 	defer { ops.pop_offset() }
-	for b in toolbar.buttons {
-		b.draw(ops, gfx)
+	for i, b in toolbar.buttons {
+		b.draw(ops, gfx, i == toolbar.active_button_id)
+		ops.push_offset((b.area.min.x + b.area.max.x) + 5, 0)
+	}
+	for i := 0; i < toolbar.buttons.len; i++ {
+		ops.pop_offset()
 	}
 }
 
 pub fn(mut toolbar Toolbar) on_event(mut ops op.Stack, e &gg.Event) bool {
 	ops.push_offset(toolbar.area.min.x, toolbar.area.min.y)
 	ops.push_offset(5, 5)
+
+	update_active := fn [mut toolbar] (id int) {
+		toolbar.active_button_id = id
+	}
+
 	mut captured := false
-	for i := toolbar.buttons.len-1; i >= 0; i-- {
-		captured = toolbar.buttons[i].on_event(ops, e)
+	mut maxi := 0
+	for i, mut b in toolbar.buttons {
+		captured = b.on_event(ops, e, i, update_active, i == toolbar.active_button_id)
+		ops.push_offset((b.area.min.x + b.area.max.x) + 5, 0)
+		maxi = i+1
 		if captured {
 			break
 		}
+	}
+	for _ in 0..maxi {
+		ops.pop_offset()
 	}
 	ops.pop_offset()
 	ops.pop_offset()
@@ -142,7 +162,6 @@ fn contains_point(min Pt, max Pt, pt Pt) bool {
 	if pt.x > min.x && pt.x < min.x + max.x && pt.y > min.y && pt.y < min.y + max.y { return true }
 	return false
 }
-
 
 fn (toolbar Toolbar) clip(posx f32, posy f32, gfx &gg.Context) {
 	gfx.scissor_rect(int(posx), int(posy), int(toolbar.area.max.x), int(toolbar.area.max.y))
